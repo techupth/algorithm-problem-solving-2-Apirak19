a6b5ed181538ab6badd4bad6e99e2eb7e9797fe8
Apirak Fakin <apirakfakin@gmail.com>
2024-07-17 14:14:07 +0700

    feat: add ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,40 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (students[mid] === searchStudent) {
+      return mid;
+    } else if (students[mid] < searchStudent) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
 
 // ตอบคำถามตรงนี้จ้า
+console.log(
+  findStudentIndex(
+    [
+      "Andrew",
+      "Bobby",
+      "Diana",
+      "George",
+      "Hannah",
+      "Isaac",
+      "Jasmine",
+      "John",
+    ],
+    "John"
+  )
+);
+
+console.log(
+  findStudentIndex(
+    ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"],
+    "Andrew"
+  )
+);
